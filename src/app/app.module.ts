121cd58d1bd66c9b5a99c04f90168f24a5777482
Jakob Buis <jakob@cubiso.nl>
2016-10-23 12:21:21 +0200

    added declarations for meal service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,28 @@ import { MyApp } from './app.component';
 import { Register } from '../pages/register/register';
 import { Login } from '../pages/login/login';
 import { OAuth } from '../services/oauth';
+import { MealService } from '../services/meal';
 import { CurrentUser } from '../services/current_user';
 
 @NgModule({
-  declarations: [
-    MyApp,
-    Register,
-    Login
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    Register,
-    Login
-  ],
-  providers: [
-      OAuth,
-    CurrentUser
-  ]
+    declarations: [
+        MyApp,
+        Register,
+        Login
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        Register,
+        Login
+    ],
+    providers: [
+        OAuth,
+        CurrentUser,
+        MealService,
+    ]
 })
 export class AppModule {}
